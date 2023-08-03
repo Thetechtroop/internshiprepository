2610e5018e23967db1bc0c33eb04a53f7c9af6d4
Thetechtroop <141333461+Thetechtroop@users.noreply.github.com>
2023-08-03 20:27:50 +0530

    Add files via upload

---
+++ userpage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<HTML LANG="EN">
+    <head>
+    <META chaset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-EDGE">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>USER PAGE</TITLE>
+    <link rel="stylesheeet" href="stylee.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="content">
+                <h3>Hi,<span>User</span></h3>
+            <h1>WELCOME<span></span></h1>
+            <P>This is an User page</P>
+            <a href="loginform.php" class="btn">Login</a>
+            <a href="register.php" class="btn">Register</a>
+            <a href="logout.php" class="btn">Logout</a>
+            </div>
+        </div>
+    </body>
